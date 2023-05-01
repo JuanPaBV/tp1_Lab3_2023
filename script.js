b5b054f85115dabccefd29cfac59269883824aba
JuanPaBV <juanpablobosio@hotmail.com>
2023-05-01 20:19:00 -0300

    btnMeGusta agregada funcionalidad

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let btnSeguir = document.getElementById('btn_seguir'); 
-btnSeguir.addEventListener('click', seguir)
+btnSeguir.addEventListener('click', seguir);
 
 function seguir() {
     if (btnSeguir.innerText == 'Dejar de seguir') {
@@ -8,3 +8,18 @@ function seguir() {
     else btnSeguir.innerText = 'Dejar de seguir';
 }
 
+let btnMeGusta = document.getElementById('btn_meGusta');
+btnMeGusta.addEventListener('click', meGusta);
+let contador = document.getElementById('contador');
+let clicked = false;
+
+function meGusta() {
+    if (!clicked) {
+        clicked = true;
+        contador.textContent++;
+    }
+    else {
+        clicked = false;
+        contador.textContent--;
+    }
+}
